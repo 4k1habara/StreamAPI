289a0a7fd4e1e44bd009e9ed41f5adbba3a71dfc
shawt <shawtyhead@mail.ru>
2025-01-09 18:35:16 +0300

    init commit

---
+++ src/main/java/com/courseworkto/streamapihw/StreamapihwApplication.java
@@ -0,0 +1,13 @@
+package com.courseworkto.streamapihw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamapihwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamapihwApplication.class, args);
+	}
+
+}
