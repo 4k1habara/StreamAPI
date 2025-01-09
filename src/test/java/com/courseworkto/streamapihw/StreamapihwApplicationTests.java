0a30e1ec66bfcd8058b5f9283f1e7e241f7795e8
shawt <shawtyhead@mail.ru>
2025-01-09 18:35:43 +0300

    init commit

---
+++ src/test/java/com/courseworkto/streamapihw/StreamapihwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseworkto.streamapihw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamapihwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
